811d4f3e348baa4af9574740b2c53736e97ee577
wontop02 <wjy0502kr@naver.com>
2026-01-05 00:07:08 +0900

    feat: 시작 문구 출력 및 실행 확인

---
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -0,0 +1,21 @@
+package bridge.controller;
+
+import bridge.service.BridgeGameService;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
+public class BridgeGameController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final BridgeGameService bridgeGameService;
+
+    public BridgeGameController(InputView inputView, OutputView outputView, BridgeGameService bridgeGameService) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.bridgeGameService = bridgeGameService;
+    }
+
+    public void run() {
+        outputView.printStart();
+    }
+}
